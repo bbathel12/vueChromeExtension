5dc20a111612bafaf9e20910357bade456912ea1
bbathel12 <bricebathel@hotmail.com>
2018-08-21 21:08:37 -0500

    initial commit

---
+++ src/components/login.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <label>URL:
+            <input v-model="url" type="text">
+        </label>
+
+        <label>API Key:
+            <input v-model="apikey" type="text">
+        </label>
+
+        <input @click="saveLogin" type="submit" value="lalalalal">
+    </div>
+</template>
+
+<script>
+export default{
+    name:"Login",
+    data(){
+        return {
+        url:'',
+        apikey:'',
+        sync:localStorage,
+        }
+    },
+    methods:{
+        saveLogin(event){
+            this.$store.commit("addAccount",{url:this.url,apikey:this.apikey});
+            var accts = this.$store.getters.accounts;
+            this.sync.setItem("accounts",JSON.stringify(accts));
+        },
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
