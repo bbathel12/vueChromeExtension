a0ef0fd97b96464b386a1e7eba2f4dff1793b584
Brice Bathel <bricebathel@hotmail.com>
2018-08-23 10:31:38 -0500

    added stylesheets

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,12 @@
 <template>
     <div id="app">
+        <link href="/assets/styles/reset.css"rel="stylesheet" type="text/css"/>
+        <link href="/assets/styles/style.css"rel="stylesheet" type="text/css"/>
+        <link href="/assets/styles/jquery/jquery.faloading.min.css"rel="stylesheet" type="text/css"/>
+        <link href="/assets/styles/plugins/font-awesome.min.css"rel="stylesheet" type="text/css"/>
+        <link href="/assets/styles/bootstrap/bootstrap.css"rel="stylesheet" type="text/css"/>
+        <link href="/assets/styles/bootstrap/bootstrap-theme.css"rel="stylesheet" type="text/css"/>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
         <router-link to="/summary/4daccb2304794d8de605aa382b1001520bfee97b76b5183a/">SUMMARY TEST</router-link>
         <router-link to="/accounts">accounts</router-link>
         <router-link to="/">Login</router-link>
@@ -24,4 +31,5 @@ export default {
     color: #2c3e50;
     margin-top: 60px;
 }
+
 </style>
--- src/components/summary.vue
+++ src/components/summary.vue
@@ -1,6 +1,45 @@
 <template>
-    <div>
-        <h1>summary</h1>
+    <div id="container">
+        <div id="header">
+            <img src="/images/extension-icon.png"/> Monthly Network Stats
+            <span id="icon_info" class="glyphicon glyphicon-info-sign" aria-hidden="true" style="float: right; mar
+            gin-top: 2px; cursor: pointer;" title="Settings"></span>
+        </div>
+        <div id="content">
+
+            <div id="content">
+                <div class="metric metric_small">
+                    <img src="https://logos.hitpath.com/a41b5fa679733fb0cc71f952586b4f02.png">
+                </div>
+                <div class="metric metric_small metric_gradient">
+                    <div class="metric_label">Hits
+                    </div>
+                    <div class="metric_value">127
+                    </div>
+                </div>
+                <div class="metric metric_small metric_gradient">
+                    <div class="metric_label">Sales
+                    </div>
+                    <div class="metric_value">63
+                    </div>
+                </div>
+                <div class="metric metric_medium metric_gradient"><span class="metric_label">Conversion </span><span class="metric_value">49.61%</span>
+                </div>
+                <div class="metric metric_medium metric_gradient"><span class="metric_label">EPC </span><span class="metric_value">$5.48</span>
+                </div>
+                <div class="metric metric_large metric_gradient"><span class="metric_label">Commission </span><span class="metric_value">$696.00</span>
+                </div>
+                <div class="clear_div">
+                </div>
+            </div>
+        </div>
+        <div id="footer">
+            <div id="network_name"></div>
+            <div id="network_login"><a href="" target="_blank" style="outline: none; color: black"><span id="icon_
+            info" class="glyphicon glyphicon-log-in" aria-hidden="true" style="cursor: pointer;" title="Login"></span></a></div>
+            <div id="last_update">Last Updated: <span id="last_update_timestamp">-</span></div>
+            <div class="clear_div"></div>
+        </div>
     </div>
 </template>
 
@@ -9,22 +48,19 @@ import axios from "axios"
 export default{
     name:"summary",
     data(){
-        return {};
+        return{
+            endpoint:"_services/dashboard_mobile.php",
+        }
     },
     mounted(){
-        console.log("HELLO MOTHER FUCKER");
         this.getData();
     },
     methods:{
         getData(){
             // save a reference to the app
             let app = this;
-            // get the parameter
-            let accountid = this.$route.params.accountid
             // get proper account
-            let account   = this.accounts[accountid];
-            // this is the page that has the data
-            let endpoint  = "_services/dashboard_mobile.php";
+            let account   = app.chosenAccount;
             // create the request with proper headers
             let request   = axios.create({
                 headers:{
@@ -38,7 +74,7 @@ export default{
             body.set("func","plugin_data");
             // post the request
             request.post(
-                account.url+"/"+endpoint,
+                account.url+"/"+app.endpoint,
                 body
             ).then( function(response){
                 app.saveAccountData(account,response.data);
@@ -57,6 +93,15 @@ export default{
         accounts(){
             return this.$store.getters.accounts;
         },
+        firstAccount(){
+            return this.$store.getters.accounts;
+        },
+        accountId(){
+            return this.$route.params.accountid;
+        }
+        chosenAccount(){
+            return this.$store.getters.accounts[this.accountId];
+        }
     },
 }
 
