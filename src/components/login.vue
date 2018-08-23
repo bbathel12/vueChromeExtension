8580b2ec46bf205d9da714f6845b6afc9361ea68
Nikita Soifer <nikita@hitpath.com>
2018-08-23 13:33:06 -0500

    Login Layout

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,19 +1,19 @@
 <template>
     <div>
         <div id="container">
-            <div id="header"><img src="/images/extension-icon.png"> Monthly Network Stats</div>
+            <div id="header"><img style="height: 20px; width:20px" src="/images/extension-icon-48x48.png"> Login</div>
             <div id="content" class="content_bump">
 
 
                 <div class="form-group">
-                    <label class="control-label col-sm-2" for="name">Url:</label>
+                    <label class="control-label col-sm-2" for="name" style="float:left">Url:</label>
                     <div class="col-sm-5">
                         <input  v-model="url" type="url" class="form-control" id="url" name="url" placeholder="Enter url" value="" autocomplete="off" required>
                         <span class="glyphicon form-control-feedback glyphicon_bump" aria-hidden="true"></span>
                     </div>
                 </div>
                 <div class="form-group">
-                    <label class="control-label col-sm-2" for="name">API Key:</label>
+                    <label class="control-label col-sm-2" for="name" style="float:left">API Key:</label>
                     <div class="col-sm-5">
                         <input type="text" v-model="apikey" class="form-control" id="key" name="key" placeholder="Enter key" value="" autocomplete="off" required>
                         <span class="glyphicon form-control-feedback glyphicon_bump" aria-hidden="true"></span>
@@ -32,7 +32,7 @@
 
             </div>
             <div id="footer">
-                <div id="general_info">For assistance logging in, please click the <img src="/images/extension-icon.png" style="margin-top: -4px;"/> extension icon in your footer.</div>
+                <div id="general_info">For assistance logging in, please click the <img src="/images/icon_gear.png"/> gear icon in your footer.</div>
                 <div class="clear_div"></div>
             </div>
         </div>
