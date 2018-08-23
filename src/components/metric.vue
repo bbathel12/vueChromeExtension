d71f2f7a69610c62c71a0b3fe32b525c75a78059
Brice Bathel <bricebathel@hotmail.com>
2018-08-23 10:47:31 -0500

    building metric

---
+++ src/components/metric.vue
@@ -0,0 +1,32 @@
+<template>
+    <div v-bind:class="[
+        {metric:true},
+        {metric:gradient},
+        {metric_small:isSmall},
+        {metric_medium:isMedium},
+        {metric_large:isLarge}
+        ]">
+
+        <div class="metric_label" v-text="metric.label">
+        </div>
+
+        <div class="metric_value" v-text="metric.value">
+        </div>
+
+    </div>
+
+</template>
+
+<script>
+export default{
+    name:"metric",
+    props:["metric"],
+    data(){
+        return {},
+    },
+}
+</script>
+
+<style>
+</style>
+
