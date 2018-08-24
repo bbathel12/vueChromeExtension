d78b8fb35860f65a378dc0a7d953b0821e2f8384
Brice Bathel <bricebathel@hotmail.com>
2018-08-24 13:51:24 -0500

    content script working

--- static/content.js
+++ static/content.js
@@ -1,8 +1,6 @@
-console.log("we made it");
-
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     // If the received message has the expected format...
-    if (msg.text === 'import_creds') {
+    if (msg.action === 'import_creds') {
         // Call the specified callback, passing
         // the web-page's DOM content as argument
         sendResponse({
