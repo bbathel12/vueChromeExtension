c69483f3ce05a13af883fb3c3d46ac0e01557da2
Nikita Soifer <nikita@hitpath.com>
2018-09-05 10:58:22 -0500

    Settings Working and Removed Layout

---
+++ src/components/accountDropDownSettings.vue
@@ -0,0 +1,40 @@
+<template>
+    <span>
+        <select @change="followLink" >
+            <option
+                v-for="account in allNamedAccounts"
+                v-if="account.network_name != undefined"
+                :value="account.apikey"
+                v-text="account.network_name">
+            </option>
+        </select>
+    </span>
+</template>
+
+<script>
+import {dataGetter} from "@/datagetter/dataGetter";
+
+export default{
+    name:"accountDropDownSettings",
+    mixins:[dataGetter],
+    data(){
+        return {};
+    },
+    methods:{
+        followLink(e){
+            this.$router.push(
+                {
+                    name:"SettingsWId",
+                    params:{
+                        accountid:e.target.value
+                    }
+                }
+            );
+            this.$emit('linkfollow');
+        },
+    },
+}
+</script>
+
+<style>
+</style>
